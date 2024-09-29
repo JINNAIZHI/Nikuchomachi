558cfdf25f049667ff8f5b78b9fcac7e6b2859d9
JINNAIZHI <kingtatum9321@gmail.com>
2024-09-30 00:41:32 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-var swiper = new Swiper('.swiper-container', {
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    loop: true,
-});
+function setLanguage(lang) {
+    const elements = document.querySelectorAll('[data-lang]');
+    elements.forEach(el => {
+        if (el.getAttribute('data-lang') === lang) {
+            el.style.display = 'block';
+        } else {
+            el.style.display = 'none';
+        }
+    });
+}
+
+// 默认显示日语
+setLanguage('ja');
